98d501410fddc02ee76b969b84fabf767a246afa
feigo808 <feigo.code@gmail.com>
2022-02-19 16:59:11 +0900

    Added Room DB related classes

---
+++ app/src/main/java/com/feiyatsu/cleannoteapp/feature_note/domain/model/Note.kt
@@ -0,0 +1,18 @@
+package com.feiyatsu.cleannoteapp.feature_note.domain.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.feiyatsu.cleannoteapp.ui.theme.*
+
+@Entity
+data class Note(
+    val title: String,
+    val content: String,
+    val timestamp: Long,
+    val color: Int,
+    @PrimaryKey val id: Int? = null
+) {
+    companion object {
+        val noteColors = listOf(RedOrange, RedPink, BabyBlue, Violet, LightGreen, LightGrey)
+    }
+}
