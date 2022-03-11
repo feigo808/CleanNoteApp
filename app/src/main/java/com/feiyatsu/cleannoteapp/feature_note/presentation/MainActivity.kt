fc995e91c8ecb2efff085a3bf91f9922524097b0
feigo808 <feigo.code@gmail.com>
2022-03-10 16:15:11 -1000

    Added noteScreen using composable

--- app/src/main/java/com/feiyatsu/cleannoteapp/feature_note/presentation/MainActivity.kt
+++ app/src/main/java/com/feiyatsu/cleannoteapp/feature_note/presentation/MainActivity.kt
@@ -2,11 +2,28 @@ package com.feiyatsu.cleannoteapp.feature_note.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 import com.feiyatsu.cleannoteapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent{
+//            Greeting("Fei")
+        }
     }
+}
+
+@Composable
+fun Greeting(name:String){
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview(){
+    Greeting("Fei")
 }
